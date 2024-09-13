8cd1564b33f3ca3c2135a63614d9e81d9802c87b
jdlee <jdlee@bmcnet.kr>
2024-09-13 16:28:12 +0900

    로그인 마크업 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,34 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="login">
+      <section>
+        <p className="login-p-1">스마트 제조 혁신</p>
+        <h1>도면 및 검사관리 솔루션 V1.0</h1>
+        <p className="login-p-2">3차원 모델링의 업무 기록으로 언제 어디서든 빠른 정보 파악과<br/>
+          기록된 업무 내용 공유를 통한 차별화된 업무 협업
+        </p>
+      </section>
+      <section>
+        <h2>로그인</h2>
+        <form>
+          <fieldset className="input-email">
+            <label htmlFor="email">이메일</label>
+            <input id="email" type="text"/>
+          </fieldset>
+          <fieldset className="input-password">
+            <label htmlFor="password">이메일</label>
+            <input id="password" type="password"/>
+          </fieldset>
+          <fieldset>
+            <Link href="#">회원가입</Link> | <Link href="#">비밀번호 찾기</Link>
+          </fieldset>
+          <input type="submit" value="로그인"/>
+        </form>
+        <p><Image src="/images/login/img_logo.svg" alt="전남대불산학융합원" width={295} height={43}/></p>
+      </section>
     </div>
   );
 }
