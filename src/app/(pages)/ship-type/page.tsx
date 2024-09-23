51566e3d13d0118517c3ec811d9c5e30fe4a327e
adrian3438 <admin@bmcnet.kr>
2024-09-23 18:03:03 +0900

    modify

--- src/app/(pages)/ship-type/page.tsx
+++ src/app/(pages)/ship-type/page.tsx
@@ -17,8 +17,9 @@ export default async function Main({searchParams : {s}} : MainProps) {
     const data = response?.data?.result === true ? response?.data?.List : []
     const shipid = s || response?.data?.List[0]?.ID?.toString() || '';
     // 대조리스트
-    const response2 = await api.get(`/admin/getShipAssembleListByShipType.php?shipTypeId=${3}`)
-    // console.log(s)
+    const response2 = await api.get(`/admin/getShipAssembleListByShipType.php?shipTypeId=${s || response?.data?.List[0]?.ID?.toString()}`)
+    const data2 = response2?.data?.result === true ? response2?.data?.List : []
+    console.log(response2?.data)
     return (
         <div className="ship-list">
 
@@ -30,7 +31,7 @@ export default async function Main({searchParams : {s}} : MainProps) {
             
             {/* 대조관리박스 */}
             <ShipAssembleBox
-
+                data={data2}
             />
         </div>
     )
--- src/components/Pages/ShipType/ship-assemble-box.tsx
+++ src/components/Pages/ShipType/ship-assemble-box.tsx
@@ -1,24 +1,34 @@
 'use client' 
 import Link from "next/link";
-
-export default function ShipAssembleBox () {
+import Image from "next/image";
+interface Props {
+    data : DataType[]
+}
+interface DataType {
+    ID : string | Blob, thumnailFile : string, thumnailFilename : string,
+    shipAssembleName : string, bopIdx : string | number , bopExist : boolean,
+    createDate : string
+}
+export default function ShipAssembleBox ({data} : Props) {
 
     return(
         <>
         <section className="ship-type-bop">
             <ul>
-                <li>
-                    <div>
-                        <div className="bop-image-area">
-                            이미지 영역
+                {data?.map((list:DataType) => (
+                    <li>
+                        <div>
+                            <div className="bop-image-area">
+                                <Image src={list?.thumnailFile ? list?.thumnailFile : '/images/no-image.jpg'} alt="sample" width={100} height={100}/>
+                            </div>
+                            <div className="bop-info-area">
+                                <p>{list?.shipAssembleName}</p>
+                                <p className="registed">BOP <Link href="/dotsAdmin">수정</Link></p>
+                            </div>
                         </div>
-                        <div className="bop-info-area">
-                            <p>S11C</p>
-                            <p className="registed">BOP <Link href="#">수정</Link></p>
-                        </div>
-                    </div>
-                </li>
-                <li>
+                    </li>
+                ))}
+                {/* <li>
                     <div>
                         <div className="bop-image-area">
                             이미지 영역
@@ -28,7 +38,7 @@ export default function ShipAssembleBox () {
                             <p className="no-registed">BOP <Link href="#">등록</Link></p>
                         </div>
                     </div>
-                </li>
+                </li> */}
             </ul>
         </section>
         </>
