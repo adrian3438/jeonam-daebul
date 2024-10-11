0f277089fbfc66455c20224a778e401959100ba9
jdlee <jdlee@bmcnet.kr>
2024-10-11 09:56:16 +0900

    editor js 입력창 라인 추가

--- src/components/NoteRegistModal.tsx
+++ src/components/NoteRegistModal.tsx
@@ -112,7 +112,7 @@ const NoteRegistModal: React.FC<CustomModalProps> = ({ shipId, listId, assembleI
                         <h3>노트 제목</h3>
                         <input type="text" value={data?.subject} onChange={(e) => setData((prev) => ({...prev, subject: e.target.value}))} placeholder='제목을 입력하세요.'/>
                     </div>
-                    <div className="change-reason4">
+                    <div className="change-reason2">
                     {initData &&
                             <Editorjs
                                 isEdit={true}
