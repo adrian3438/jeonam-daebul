093a65f5b0b7c8e2eaf99615f33fcb3fe82b5041
adrian3438 <admin@bmcnet.kr>
2024-10-21 10:09:20 +0900

    관리자 리스트 & 상태변경 & 등록 & 수정

--- src/components/Pages/Setting/admin-list.tsx
+++ src/components/Pages/Setting/admin-list.tsx
@@ -1,30 +1,54 @@
 'use client'
 
+import calCulateIndex from "@/components/calculateIndex";
 import SettingModal from "@/components/SettingModal";
+import api from "@/lib/api";
 import { useEffect, useState } from "react";
-
+interface ManagerType {
+    ID : number
+    activeStatus : string
+    managerDept : string
+    managerEmail : string
+    managerLogin : string
+    managerMobile : string
+    managerName : string
+    managerPhone : string
+    managerPosition : string
+}
 export default function AdminList () {
     const [id , setId] = useState<number>(0)
+    const [data , setData] = useState<ManagerType[]>([])
+    const [totalCount , setTotalCount] = useState<number>(0)
+
     const [modalIsOpen1, setModalIsOpen1] = useState(false);
     const [contentLabel, setContentLabel] = useState("");
     const openModal1 = () => {
         setModalIsOpen1(true);
     };
 
-    const openModal2 = () => {
+    const openModal2 = (id : number) => {
+        setId(id)
         setModalIsOpen1(true);
     };
 
     const closeModal = () => {
+        setId(0)
         setModalIsOpen1(false);
     };
     
     async function getList () {
-
+        const response = await api.get(`/admin/manager/getManagerList.php?page=1&size=10&keyword=&sortColumn=managerName&sortOrder=desc`)
+        if(response?.data?.Result === true) {
+            setData(response?.data?.List); setTotalCount(response?.data?.totalCnt)
+        }
     }
 
-    async function ChangeStaus (id : number , status : string) {
-        
+    async function ChangeStaus (id : number | any , status : string) {
+        const formData = new FormData()
+        formData.append('ID' , id)
+        formData.append('activeStatus', status === 'Y' ? 'N' : 'Y')
+        const response = await api.post(`/admin/manager/updActiveStatus.php`, formData)
+        if(response?.data?.result === true) { getList() }
     }
 
     useEffect(()=> {
@@ -49,25 +73,32 @@ export default function AdminList () {
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>홍길동</td>
-                    <td>마케팅</td>
-                    <td>test</td>
-                    <td>010-0000-2222</td>
-                    <td>052-2323-2222</td>
-                    <td>
-                        <label className="toggle_switch">
-                            <input
-                                type="checkbox"
-                            />
-                            <span className="slider"></span>
-                        </label>
-                    </td>
-                    <td>
-                        <button className="btn2" onClick={openModal2}>수정</button>
-                    </td>
-                </tr>
+                    {data?.map((list:ManagerType, index:number) => {
+                        const indexNumber = calCulateIndex(1, 10, totalCount, index);
+                        return(
+                            <tr key={index}>
+                                <td>{indexNumber}</td>
+                                <td>{list?.managerName}</td>
+                                <td>{list?.managerDept}</td>
+                                <td>{list?.managerLogin}</td>
+                                <td>{list?.managerPhone}</td>
+                                <td>{list?.managerMobile}</td>
+                                <td>
+                                    <label className="toggle_switch">
+                                        <input
+                                            type="checkbox"
+                                            checked={list?.activeStatus === 'Y'}
+                                            onChange={()=>ChangeStaus(list?.ID, list?.activeStatus)}
+                                        />
+                                        <span className="slider"></span>
+                                    </label>
+                                </td>
+                                <td>
+                                    <button className="btn2" onClick={()=>openModal2(list?.ID)}>수정</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
             <SettingModal 
--- src/components/SettingModal.tsx
+++ src/components/SettingModal.tsx
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Modal from 'react-modal';
 import Image from "next/image";
 import '@/app/assets/modal.scss';
 import Link from "next/link";
+import api from '@/lib/api';
 
 const customStyles = {
     content: {
@@ -20,13 +21,93 @@ interface CustomModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
     contentLabel: string;
-    id : number
+    id : any
+}
+
+interface ManagerType {
+    ID : number
+    activeStatus : string
+    managerDept : string
+    managerEmail : string
+    managerLogin : string
+    managerMobile : string
+    managerName : string
+    managerPhone : string
+    managerPosition : string
+    managerPass : string
+    managerPassConfirm : string
 }
 
 const SettingModal: React.FC<CustomModalProps> = ({ isOpen, onRequestClose, contentLabel , id }) => {
-    const [data, setData] = useState<any>({
-        
+    const [data, setData] = useState<ManagerType>({
+        ID : 0 , activeStatus : '', managerDept : '', managerEmail : '', managerLogin : '', managerMobile : '',
+        managerName : '', managerPhone : '', managerPosition : '' , managerPass : '', managerPassConfirm : ''
     })
+    
+    function handleChange (e:React.ChangeEvent<HTMLInputElement>) {
+        const {name , value} = e.target;
+        setData((prev:any) => ({...prev, [name] : value}))
+    }
+    async function Save () {
+        if(!data?.managerLogin) {alert('아이디를 입력해 주세요.'); return;}
+        if(!data?.managerName) {alert('이름을 입력해 주세요.'); return;}
+        if(!data?.managerDept) {alert('부서를 입력해 주세요.'); return;}
+        if(!data?.managerEmail) {alert('이메일을 입력해 주세요.'); return;}
+        const formData = new FormData();
+        if(id!==0) {formData.append('ID', id)}
+        formData.append('managerLogin' , data?.managerLogin)
+        if(data?.managerPass) {
+            if(data?.managerPass === data?.managerPassConfirm){
+                formData.append('managerPass' , data?.managerPass)
+            }else{
+                alert('비밀번호를 재확인해주시기 바랍니다.'); return;
+            }
+        }else{
+            if(id === 0) {
+                alert('비밀번호를 입력해주시기 바랍니다.'); return;
+            }
+        }
+        formData.append('managerName' , data?.managerName)
+        formData.append('managerPhone', data?.managerPhone)
+        formData.append('managerMobile', data?.managerMobile)
+        formData.append('managerEmail', data?.managerEmail)
+        formData.append('managerPosition', data?.managerPosition)
+        formData.append('managerDept' , data?.managerDept)
+        if(id === 0) {
+            const response = await api.post(`/admin/manager/setManager.php`, formData)
+            if(response?.data?.result === true) {
+                alert(response?.data?.resultMsg); onRequestClose()
+            }else{
+                alert(response?.data?.resultMsg)
+            }
+        }else{
+            const response = await api.post(`/admin/manager/updManager.php`, formData)
+            if(response?.data?.result === true) {
+                alert(response?.data?.resultMsg); onRequestClose()
+            }else{
+                alert(response?.data?.resultMsg)
+            }
+        }
+    }
+    console.log(data)
+    useEffect(() => {
+        async function getDetail () {
+            if(id !== 0) {
+                const response = await api.get(`/admin/manager/getManagerDetail.php?ID=${id}`)
+                if(response?.data?.result === true) {
+                    if(response?.data?.List?.length > 0) {
+                        setData(response?.data?.List[0])
+                    }
+                }
+            }else{
+                setData({
+                    ID : 0 , activeStatus : '', managerDept : '', managerEmail : '', managerLogin : '', managerMobile : '',
+                    managerName : '', managerPhone : '', managerPosition : '' , managerPass : '', managerPassConfirm : ''
+                })
+            }
+        }
+        getDetail()
+    }, [id])
     return (
         <Modal
             isOpen={isOpen}
@@ -45,56 +126,56 @@ const SettingModal: React.FC<CustomModalProps> = ({ isOpen, onRequestClose, cont
                         <tr>
                             <th scope="row">이름 <span>*</span></th>
                             <td>
-                                <input type="text"/>
+                                <input type="text" name='managerName' value={data?.managerName} onChange={handleChange}/>
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">부서 <span>*</span></th>
                             <td>
-                                <input type="text"/>
+                                <input type="text" name='managerDept' value={data?.managerDept} onChange={handleChange}/>
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">아이디 <span>*</span></th>
                             <td>
-                                <input type="text"/>
+                                <input type="text" name='managerLogin' value={data?.managerLogin} onChange={handleChange}/>
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">이메일 <span>*</span></th>
                             <td>
-                                <input type="text"/>
+                                <input type="text" name='managerEmail' value={data?.managerEmail} onChange={handleChange}/>
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">휴대전화 <span>*</span></th>
                             <td>
-                                <input type="text"/>
+                                <input type="text" name='managerPhone' value={data?.managerPhone} onChange={handleChange}/>
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">전화번호 <span>*</span></th>
                             <td>
-                                <input type="text"/>
+                                <input type="text" name='managerMobile' value={data?.managerMobile} onChange={handleChange}/>
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">비밀번호 <span>*</span></th>
                             <td>
-                                <input type="text"/>
+                                <input type="password" name='managerPass' value={data?.managerPass} onChange={handleChange}/>
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">비밀번호 확인 <span>*</span></th>
                             <td>
-                                <input type="text"/>
+                                <input type="password" name='managerPassConfirm' value={data?.managerPassConfirm} onChange={handleChange}/>
                             </td>
                         </tr>
                         </tbody>
                     </table>
                 </div>
                 <div className='btns7'>
-                    <button>저장</button>
+                    <button onClick={()=>Save()}>저장</button>
                 </div>
             </div>
         </Modal>
